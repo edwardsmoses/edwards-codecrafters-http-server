10c503c7e4fd300a7df2f7bb4aacf4627f6d270e
Edwards Moses <edwardsmoses3@gmail.com>
2024-04-01 20:41:13 +0000

    updates to passing 1st stage

--- app/server.go
+++ app/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
@@ -11,7 +10,6 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
 	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -19,7 +17,7 @@ func main() {
 		os.Exit(1)
 	}
 	//
-	_, err = l.Accepst()
+	_, err = l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
